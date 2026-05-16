f9cf4ced70282caf64a28ae81986d8fbff46b914
kail2021 <kail2021@outlook.com>
2026-05-16 03:22:06 -0700

    ProtectingImages

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import "./index.css";
 import "./i18n";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+
+
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
     <App />
